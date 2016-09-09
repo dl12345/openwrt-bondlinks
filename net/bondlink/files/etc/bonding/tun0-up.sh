d1e3a465a6c21178d63e36dcc75c61acf33b2b89
DL <dl12345@github.com>
2016-09-09 21:16:11 +0700

    Update tunX-up.sh and tunX-down.sh scripts to add and remove tunnel interfaces as slave devices

--- net/bondlink/files/etc/bonding/tun0-up.sh
+++ net/bondlink/files/etc/bonding/tun0-up.sh
@@ -1,3 +1,29 @@
 #!/bin/sh
 
-exit 0
+BONDIF="bond0"
+TUN=${1}
+SLAVES=$(cat /sys/class/net/${BONDIF}/bonding/slaves)
+
+# If it's already a slave then do nothing
+
+for i in ${SLAVES}; do
+
+	if [ $i == ${TUN} ]; then
+	
+		exit 0
+
+	fi
+
+done
+
+# Otherwise add it as a slave - this will occur if the link goes down and openvpn re-establishes it
+# since we're removing it as a slave on a tunnel down event. 
+
+ifconfig ${1} down 2> /dev/null
+echo "+${1}" > /sys/class/net/${BONDIF}/bonding/slaves 2> /dev/null
+ifconfig ${1} up 2> /dev/null
+
+
+exit $?
+
+
--- net/bondlink/files/etc/bonding/tun1-down.sh
+++ net/bondlink/files/etc/bonding/tun1-down.sh
@@ -1,3 +1,26 @@
 #!/bin/sh
 
+BONDIF="bond0"
+TUN=${1}
+SLAVES=$(cat /sys/class/net/${BONDIF}/bonding/slaves)
+
+# Check if it's a slave and if so then remove it from the list of slaves
+# Failure to do so will result in packet loss as the bonding driver continues 
+# to round robin packets onto a disconnected interface
+
+for i in ${SLAVES}; do
+
+	if [ $i == ${TUN} ]; then
+	
+		ifconfig ${1} down 2> /dev/null
+		echo "-${1}" > /sys/class/net/${BONDIF}/bonding/slaves 2> /dev/null
+
+		exit $?
+
+	fi
+
+done
+
+# Otherwise do nothing
+
 exit 0
